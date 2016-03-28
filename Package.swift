62cc91f591c80abe81a2f344c65cbc91a0c681d7
tak <tak.msk2580@gmail.com>
2016-03-28 18:50:51 +0900

    mod server running

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "first_Kitura"
+    name: "first_Kitura",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 5)
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import Kitura
+import KituraNet
+import KituraSys
+
+let router = Router()
+
+router.get("/") { reqest, response, next in
+    response.send("Hello, World!")
+    next()
+}
+
+let server = HttpServer.listen(8090, delegate: router)
+Server.run()
